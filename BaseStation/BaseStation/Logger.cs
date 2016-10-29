5380f9ddcaa23a93649d6b342706c4cf7f7cb6f6
William Jen <wjen@wisc.edu>
2016-10-29 16:06:21 -0500

    Initial GUI creation, with logger mostly complete.

---
+++ BaseStation/BaseStation/Logger.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace BaseStation
+{
+    enum LoggerLevel
+    {
+        Info,
+        Warning,
+        Error
+    }
+
+    class Logger : IDisposable
+    {
+        private static Logger instance;
+        private static readonly Dictionary<LoggerLevel, string> levelMap = new Dictionary<LoggerLevel, string> {
+            { LoggerLevel.Info, "INFO" },
+            { LoggerLevel.Warning, "WARN" },
+            { LoggerLevel.Error, "ERROR" }
+        };
+
+        private StreamWriter writer;
+        private StringBuilder text;
+
+        /// <summary>
+        /// Gets or creates a Logger instance.
+        /// </summary>
+        /// <param name="filePath">The log file path, or null for an in-memory log.</param>
+        /// <returns>The created or existing Logger instance.</returns>
+        public static Logger GetInstance(string filePath = null)
+        {
+            if (instance == null)
+                instance = new Logger(filePath);
+
+            return instance;
+        }
+
+        private Logger(string filePath)
+        {
+            text = new StringBuilder();
+
+            if (filePath == null)
+                writer = new StreamWriter(filePath);
+        }
+
+        public void Write(LoggerLevel level, string log_text)
+        {
+            string converted_text = string.Format("[{0} | {1}] {2}\n", DateTime.Now.ToString("HH:mm:ss") , levelMap[level], log_text);
+            text.Append(converted_text);
+
+            if (writer != null)
+            {
+                writer.Write(converted_text);
+                writer.Flush();
+            }
+        }
+
+        public string Text
+        { 
+            get { return text.ToString(); }
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing && writer != null)
+                {
+                    writer.Flush();
+                    writer.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
+    }
+}
---
+++ BaseStation/BaseStation/LoggerControl.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BaseStation
+{
+    /// <summary>
+    /// Interaction logic for Logger.xaml
+    /// </summary>
+    public partial class LoggerControl : UserControl
+    {
+        private Logger log;
+
+        public LoggerControl()
+        {
+            InitializeComponent();
+            log = Logger.GetInstance(DateTime.Now.ToString("MMddyyyy_HHmmss") + ".log");
+            DataContext = log;
+
+            log.Write(LoggerLevel.Info, "Base Station GUI Logger initialized.");
+        }
+    }
+}
