08e9f088a070737ef7a09aba340ec2e6232c1e83
William Jen <wjen@wisc.edu>
2017-04-28 03:06:28 +0000

    Camera pulls latest frame via mutex & thread.

--- Robot/include/BLER.hpp
+++ Robot/include/BLER.hpp
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include <memory>
+#include <mutex>
 #include <thread>
 #include "Packet.h"
 #include "Serial.hpp"
@@ -25,41 +26,44 @@ namespace LAME
 
     class BLER
     {
-    public:
-		BLER(int base_port, int ai_port, std::string& serial_port);
-        ~BLER();
+        public:
+            BLER(int base_port, int ai_port, std::string& serial_port);
+            ~BLER();
 
-        void SendPacketSerial(const uint8_t *buffer, int length);
-		void SendPacketUdp(const uint8_t *buffer, int length, struct sockaddr *addr);
-        bool Run();
-        
-    private:
-        void ReceivePacketsFromSerial();
-        void ReceivePacketsFromUdp();
-        void Execute();
-        void QueryHeartbeatAI();
+            void SendPacketSerial(const uint8_t *buffer, int length);
+            void SendPacketUdp(const uint8_t *buffer, int length, struct sockaddr *addr);
+            bool Run();
 
-        bool isRunning;
-        bool aiConnected;
-		struct sockaddr_in baseStationAddr;
-        struct sockaddr_in aiAddr;
-        DriveMode currentMode;
+        private:
+            void ReceivePacketsFromSerial();
+            void ReceivePacketsFromUdp();
+            void Execute();
+            void QueryHeartbeatAI();
+            void CameraReadThread();
 
-        std::unique_ptr<SerialPort> serialPort;
-        std::unique_ptr<UdpSocket> socket;
-        Xbox360Service& js = Xbox360Service::GetInstance();
+            bool isRunning;
+            bool aiConnected;
+            struct sockaddr_in baseStationAddr;
+            struct sockaddr_in aiAddr;
+            DriveMode currentMode;
 
-        std::thread serialReadThread;
-        std::thread udpReadThread;
-        std::thread executeThread;
-        std::thread aiHeartbeatThread;
+            std::unique_ptr<SerialPort> serialPort;
+            std::unique_ptr<UdpSocket> socket;
+            Xbox360Service& js = Xbox360Service::GetInstance();
 
-        cv::VideoCapture camera;
+            std::thread serialReadThread;
+            std::thread udpReadThread;
+            std::thread executeThread;
+            std::thread aiHeartbeatThread;
+            std::thread cameraReadThread;
+            std::mutex cameraMutex;
 
-		// packet payload structs //
-		DrivePayload latestDrivePayload;
-		time_t lastDrivePayloadReceivedTime;
+            cv::VideoCapture camera;
+            cv::Mat latestCameraFrame;
 
-        time_t lastAIHeartbeatReceivedTime;
+            // packet payload structs //
+            DrivePayload latestDrivePayload;
+            time_t lastDrivePayloadReceivedTime;
+            time_t lastAIHeartbeatReceivedTime;
     };
 }
